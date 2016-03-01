c30dc3bf05052756899d98318ea46c86a5ae4632
kensun0 <kensun0@users.noreply.github.com>
2016-03-01 21:38:22 +0800

    Update randomforest.cpp

--- randomforest.cpp
+++ randomforest.cpp
@@ -110,6 +110,8 @@ int my_cmp(pair<float,int> p1, pair<float,int> p2)
 
 void RandomForest::Train(
                          const vector<Mat_<uchar>>& images,
+						 vector<Mat_<uchar>>& scale_map,
+						 vector<int>& find_times,
 						 const vector<int>& augmented_images,
                          vector<Mat_<float>>& ground_truth_shapes,
 						 const vector<int>& ground_truth_faces,
@@ -120,7 +122,8 @@ void RandomForest::Train(
                          const Mat_<float>& mean_shape,
                          vector<Mat_<float> >& shapes_residual,
                          int stages,
-                         vector<RandomForest>& RandomForest_){
+                         vector<RandomForest>& RandomForest_,
+						 vector<vector<struct model*>> Models_,int posLenth){
     stages_ = stages;
 	// all training samples
 	int dbsize = (int)augmented_images.size();
@@ -134,15 +137,24 @@ void RandomForest::Train(
     for (int i=0;i<num_landmark_;i++){
         clock_t tt = clock();
         for (int j =0;j <max_numtrees_; j++){
+			clock_t ss = clock();
+			//
+			/*index.clear();
+			is = max( (double)floor(j*Q - j*Q*overlap_ratio_ ), 0.0);
+			ie = min(is + Q, dbsize-1);
+			for (int k = is; k<=ie;k++){
+				index.push_back(k);
+			}*/
 			// update weight
 			#pragma omp parallel for
 			for(int k=0;k<current_weight.size();++k)
 			{
 				current_weight[k]=exp(0.0-ground_truth_faces[k]*current_fi[k]);
+				//current_weight[k]=1;
 			}
 			// build RF
-            rfs_[i][j].Train(images, augmented_images, ground_truth_shapes, ground_truth_faces , current_shapes, current_fi, current_weight, bounding_boxs, mean_shape,shapes_residual,index,stages_,i);
-
+            rfs_[i][j].Train(images, find_times, augmented_images, ground_truth_shapes, ground_truth_faces , current_shapes, current_fi, current_weight, bounding_boxs, mean_shape,shapes_residual,index,stages_,i);
+			
 			// compute all samples's fi
 			#pragma omp parallel for
 			for (int n=0;n<augmented_images.size();++n)
@@ -155,32 +167,62 @@ void RandomForest::Train(
 				GetResultfromTree(rfs_[i][j],images[augmented_images[n]],current_shapes[n],bounding_boxs[n],rotation,scale,&bincode,&score);
 				current_fi[n] = current_fi[n] + score;
 			}
-			//float fiT=0,fiF=0;
-			//float weightT=0,weigthF=0;
-			////#pragma omp parallel for
-			//for(int n=0;n<current_fi.size();++n)
-			//{
-			//	if (ground_truth_faces[n]==1)
-			//	{
-			//		fiT+=current_fi[n];
-			//		weightT+=current_weight[n];
-			//	} 
-			//	else
-			//	{
-			//		fiF+=current_fi[n];
-			//		weigthF+=current_weight[n];
-			//	}
-			//}
-			//cout<<"fiTsum:"<<fiT<<" fiFsum:"<<fiF<<endl;
-			//cout<<"weightTsum:"<<weightT<<" weightFsum:"<<weigthF<<endl;
-			
+			float fiT=0,fiF=0;
+			float weightT=0,weigthF=0;
+			int numT=0,numF=0;
+			double maxF=DBL_MIN,minF=DBL_MAX;
+			double maxW=DBL_MIN,minW=DBL_MAX;
+
+			for(int n=0;n<current_fi.size();++n)
+			{
+				if (find_times[augmented_images[n]]<=MAXFINDTIMES)
+				{
+					if (ground_truth_faces[n]==1)
+					{
+						fiT+=current_fi[n];
+						weightT+=current_weight[n];
+						++numT;
+					} 
+					else
+					{
+						fiF+=current_fi[n];
+						weigthF+=current_weight[n];
+						++numF;
+					}
+					if (current_fi[n]>maxF)
+					{
+						maxF=current_fi[n];
+					}
+					if (current_fi[n]<minF)
+					{
+						minF=current_fi[n];
+					}
+					if (current_weight[n]>maxW)
+					{
+						maxW=current_weight[n];
+					}
+					if (current_weight[n]<minW)
+					{
+						minW=current_weight[n];
+					}
+				}
+			}
+			//cout<<"score:"<<current_fi[0]<<endl;
+			cout<<"fiTsum:"<<fiT<<" fiFsum:"<<fiF<<endl;
+			cout<<"weightTsum:"<<weightT<<" weightFsum:"<<weigthF<<endl;
+			cout<<"max     fi:"<<maxF<<"\t\tmin     fi:"<<minF<<endl;
+			cout<<"max weight:"<<maxW<<"\t\tmin weight:"<<minW<<endl;
+			cout<<"avg     fiT:"<<    fiT/numT<<"\t\tavg     fiF:"<<fiF/numF<<endl;
+			cout<<"avg weightT:"<<weightT/numT<<"\t\tavg weightF:"<<weigthF/numF<<endl;
+
 			// remove false samples according to precision-recall 
 			// sort fi with index
 			vector<pair<float,int>> fiSort;
 			fiSort.clear();
 			for(int n=0;n<current_fi.size();++n)
 			{
-				fiSort.push_back(pair<float,int>(current_fi[n],n));
+				if (find_times[augmented_images[n]]<=MAXFINDTIMES)
+					fiSort.push_back(pair<float,int>(current_fi[n],n));
 			}
 			// ascent , small fi means false sample
 			sort(fiSort.begin(),fiSort.end(),my_cmp);
@@ -190,7 +232,7 @@ void RandomForest::Train(
 			int idx_tmp=-1;
 
 			vector<pair<float,float>> precise_recall;
-			for (int n=1;n<fiSort.size();++n)
+			for (int n=0;n<fiSort.size();++n)
 			{
 				int true_pos=0;int false_neg=0;
 				int true_neg=0;int false_pos=0;
@@ -228,67 +270,144 @@ void RandomForest::Train(
 					max_recall=true_pos/(true_pos+false_neg+FLT_MIN);
 					precise_recall.push_back(pair<float,float>(true_pos/(true_pos+false_neg+FLT_MIN),false_pos/(false_pos+true_neg+FLT_MIN)));
 					rfs_[i][j].threshold=fiSort[n].first;
+					//idx_tmp=n;
 				}
 				else
 					break;
 			}
 			
-			cout<<"pre_recall in tree:"<<precise_recall[precise_recall.size()-1].first<<"  "<<precise_recall[precise_recall.size()-1].second<<endl;
-			
-			//remove false samples , perform hard sample
-			int remove_num=0;
-			//#pragma omp parallel for
-			for(int n=0;n<current_fi.size();++n)
+			/*
+			for(int n=0;n<precise_recall.size();++n)
 			{
-				if (current_fi[n]<rfs_[i][j].threshold && ground_truth_faces[n]==-1)
+				if (precise_recall[n].first>=max_recall)
 				{
-					int nSelected=0;
+					max_recall=precise_recall[n].first;
+					idx_tmp=n;
+					
+					rfs_[i][j].threshold=fiSort[n].first;
+				}
+			}*/
+			cout<<"pre_recall in tree:"<<precise_recall[precise_recall.size()-1].first<<"  "<<precise_recall[precise_recall.size()-1].second<<endl;
+			//system("pause");
+			//remove false samples , perform hard sample
+
+			int useful_neg=0;
+			for (int n=posLenth;n<find_times.size();++n )
+			{
+				if (find_times[augmented_images[n]]<=MAXFINDTIMES)
+				{
+					++useful_neg;
+				}
+			}
+			cout<<"useful neg sample: "<<useful_neg<<"   percent:"<<useful_neg*1.0/(find_times.size()-posLenth)<<endl;
+			int remove_num=0;
+			#pragma omp parallel for reduction(+:remove_num)
+			for(int n=posLenth; n<current_fi.size();++n)
+			{
+				if (current_fi[n]<rfs_[i][j].threshold && ground_truth_faces[n]==-1 && find_times[augmented_images[n]]<=MAXFINDTIMES)
+				{
+					/*bool ishard=false;*/
+					//int nSelected=0;
 					while(1)
 					{
+						//if (find_times[augmented_images[n]]>MAXFINDTIMES)
+						//{
+						//	//cout<<"warning: max find times\n";
+						//	break;
+						//}
 						RNG random_generator(getTickCount());
 						int tmp_idx=n;
-
 						BoundingBox new_box;
-						while (tmp_idx==n || ground_truth_faces[tmp_idx]==1)
+						/*while (tmp_idx==n || ground_truth_faces[tmp_idx]==1)
 						{
 							tmp_idx = random_generator.uniform(0.0,ground_truth_faces.size()-1);
-						}
-						nSelected++;
-						if (nSelected>=images[augmented_images[n]].rows*images[augmented_images[n]].cols*1.0)
+						}*/
+						
+						//imshow("cur1",images[augmented_images[n]]);
+						//nSelected++;
+
+						bool map_find=false;
+						for (int rr=0;rr<scale_map[augmented_images[n]].rows;++rr)
 						{
-							Mat_<uchar> newImg = images[augmented_images[n]];
-							if(random_generator.uniform(0.0,1.0)>0.8)
+							for (int cc=0;cc<scale_map[augmented_images[n]].cols;++cc)
 							{
-								resize(images[augmented_images[tmp_idx]],newImg,newImg.size());
-							}else
+								int map_value=(int)scale_map[augmented_images[n]](rr,cc);
+								if(map_value+rr<scale_map[augmented_images[n]].rows &&
+									map_value+cc<scale_map[augmented_images[n]].cols)
+								{
+									new_box.start_x=rr;
+									new_box.start_y=cc;
+									new_box.width=map_value;
+									new_box.height=map_value;
+									new_box.centroid_x=rr+map_value/2.0;
+									new_box.centroid_y=cc+map_value/2.0;
+									map_value=map_value*1.1;
+									if (map_value>255)
+									{
+										map_value=255;
+									}
+									scale_map[augmented_images[n]](rr,cc)=(uchar)map_value;
+									map_find=true;
+									break;
+								}
+							}
+							if(map_find)
+								break;
+						}
+						if (map_find==false)
+						{
+							find_times[augmented_images[n]]=MAXFINDTIMES+1;
+						}
+						if (find_times[augmented_images[n]]>MAXFINDTIMES)
+						{
+							Mat_<uchar> newImg = images[augmented_images[n]].clone();
+							if(random_generator.uniform(0.0,1.0)>0.6)
 							{
-								Point center = Point( images[augmented_images[n]].cols/2, images[augmented_images[n]].rows/2 );
-								double angle = random_generator.uniform(0.0,360.0);
-								double scale = 1.0;
-								Mat_<float> rot_mat=getRotationMatrix2D(center,angle,scale);
-								warpAffine( images[augmented_images[n]], newImg, rot_mat, newImg.size());
-								rot_mat.release()；
-							}// else read a new image, then resize it to images[augmented_images[n]]
+								//imshow("11",newImg);
+								//resize(images[augmented_images[tmp_idx]],newImg,newImg.size());
+								//imshow("12",newImg);
+								break;
+							
+							}/*else
+							{*/
+							Point center = Point( images[augmented_images[n]].cols/2, images[augmented_images[n]].rows/2 );
+							double angle = random_generator.uniform(0.0,360.0);
+							double scale = 1.0;
+							Mat_<float> rot_mat=getRotationMatrix2D(center,angle,scale);
+							//imshow("22",newImg);
+							warpAffine( images[augmented_images[n]], newImg, rot_mat, newImg.size());
+							rot_mat.release();
+								//imshow("23",newImg);
+							//}
 
 							for (int i2=0;i2<images[augmented_images[n]].rows;++i2)
 								for(int j2=0;j2<images[augmented_images[n]].cols;++j2)
 									images[augmented_images[n]].data[i2*images[augmented_images[n]].cols+j2]=newImg.data[i2*images[augmented_images[n]].cols+j2];
-							newImg.release();
-							nSelected=0;
+							//newImg.release();
+							
+							find_times[augmented_images[n]]=0;
+							for (int m2=0;m2<scale_map[augmented_images[n]].rows;++m2)
+								for (int n2=0;n2<scale_map[augmented_images[n]].cols;++n2)
+								{
+									scale_map[augmented_images[n]](m2,n2)=MINHEIGHT;
+								}
 						}
-
-
-						getRandomBox(images[augmented_images[n]],bounding_boxs[tmp_idx], new_box);
-						Mat_<float> temp = ProjectShape(ground_truth_shapes[tmp_idx], bounding_boxs[tmp_idx]);
-						ground_truth_shapes[n] = ReProjectShape(temp, new_box);
+						//imshow("cur2",images[augmented_images[n]]);
+						//waitKey(0);
 						
-						temp = ProjectShape(current_shapes[tmp_idx], bounding_boxs[tmp_idx]);
-						current_shapes[n]=ReProjectShape(temp, new_box);
+						//getRandomBox(images[augmented_images[n]],bounding_boxs[tmp_idx], new_box);
+						
+						Mat_<float> temp1 = ProjectShape(ground_truth_shapes[tmp_idx], bounding_boxs[tmp_idx]);
+						ground_truth_shapes[n] = ReProjectShape(temp1, new_box);
+						
+						Mat_<float> temp2 = ProjectShape(current_shapes[tmp_idx], bounding_boxs[tmp_idx]);
+						current_shapes[n]=ReProjectShape(temp2, new_box);
 
 						bounding_boxs[n]=new_box;
-						
+						//cout<<"xixi";
 						bool tmp_isface=true;
 						float tmp_fi=0;
+						
 						for (int s=0;s<=stages;++s)
 						{
 							int iRange=RandomForest_[s].rfs_.size()-1;
@@ -296,14 +415,14 @@ void RandomForest::Train(
 							{
 								iRange=i;
 							}
-							for (int r=0;r<iRange;++r)
+							for (int r=0;r<=iRange;++r)
 							{
 								int jRange=RandomForest_[s].rfs_[r].size()-1;
 								if (r==i && s==stages)
 								{
 									jRange=j;
 								}
-								for (int t=0;t<jRange;++t)
+								for (int t=0;t<=jRange;++t)
 								{
 									//get score 
 									Mat_<float> rotation;
@@ -311,6 +430,11 @@ void RandomForest::Train(
 									SimilarityTransform(ProjectShape(current_shapes[n],bounding_boxs[n]),mean_shape,rotation,scale);
 									int bincode=0;
 									float score=0;
+									//cout<<s<<" "<<iRange<<" "<<jRange<<endl;
+									
+									//printf("haha in %d %d %d %d\n",s,r,t,n);
+									//char lll;cin>>lll;
+									//cvWaitKey(0);
 									GetResultfromTree(RandomForest_[s].rfs_[r][t],images[augmented_images[n]],current_shapes[n],bounding_boxs[n],rotation,scale,&bincode,&score);
 									tmp_fi+=score;
 									if (tmp_fi<RandomForest_[s].rfs_[r][t].threshold)
@@ -329,8 +453,8 @@ void RandomForest::Train(
 								Mat_<float> rotation;
 								float scale;
 								SimilarityTransform(ProjectShape(current_shapes[n],bounding_boxs[n]),mean_shape,rotation,scale);
-								for (int j =0; j <RandomForest_[s-1].num_landmark_; j++){
-									GetCodefromRandomForestOnlyOnce(binfeatures[0], j*RandomForest_[s-1].max_numtrees_,RandomForest_[s-1].rfs_[j], images[augmented_images[n]], current_shapes[n],
+								for (int j1 =0; j1 <RandomForest_[s-1].num_landmark_; j1++){
+									GetCodefromRandomForestOnlyOnce(binfeatures[0], j1*RandomForest_[s-1].max_numtrees_,RandomForest_[s-1].rfs_[j1], images[augmented_images[n]], current_shapes[n],
 										bounding_boxs[n], rotation, scale);
 								}
 								binfeatures[0][RandomForest_[s-1].num_landmark_ * RandomForest_[s-1].max_numtrees_].index = -1;
@@ -340,10 +464,12 @@ void RandomForest::Train(
 								delete[] binfeatures;
 							}
 						}
+						current_fi[n]=tmp_fi;
+						current_weight[n]=exp(0.0-ground_truth_faces[n]*current_fi[n]);
+						//box[n]=newbox
+						find_times[augmented_images[n]]=find_times[augmented_images[n]]+1;
 						if (tmp_isface)
 						{
-							current_fi[n]=tmp_fi;
-							current_weight[n]=exp(0.0-ground_truth_faces[n]*current_fi[n]);
 							break;
 						}
 					}
@@ -351,6 +477,8 @@ void RandomForest::Train(
 				} 
 			}
 			cout<<"remove "<<remove_num<<" samples\n";
+			float time = float(clock() - ss) / CLOCKS_PER_SEC;
+			cout<<"stage: "<<stages<<" landmark: "<<i<<" tree: "<<j<<" time:"<<time<<endl;
 		}
         float time = float(clock() - tt) / CLOCKS_PER_SEC;
         cout << "the train rf of "<< i <<"th landmark cost "<< time<<"s"<<endl;
