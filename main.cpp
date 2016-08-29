9aa9289819d95e5733a36936dedff66fedf581e4
kensun0 <kensun0@users.noreply.github.com>
2016-08-29 20:22:02 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,252 +1,153 @@
-//
-//  main.cpp
-//  myopencv
-//
-//  Created by lequan on 1/24/15.
-//  Copyright (c) 2015 lequan. All rights reserved.
-//
-
 #include "LBF.h"
 #include "LBFRegressor.h"
-//#include "implement.h"
 using namespace std;
 using namespace cv;
 
 // parameters
 Params global_params;
-
-
-string modelPath ="F:\\";
-string dataPath = "F:\\";
-string cascadeName = "haarcascade_frontalface_alt.xml";   // no used
-
-void InitializeGlobalParam();
-void PrintHelp();
+string modelPath = "model";
+string dataPath = "regress";
 
 int main( int argc, const char** argv ){
+	
 	InitializeGlobalParam();
-	
-	string listT="F:\\listT.txt";       //address of postive samples
-	string listF="F:\\listF.txt";       //address of negative samples
-	
-	vector<Mat_<uchar> > images_train;
-	vector<Mat_<float> > ground_truth_shapes_train;
-	vector<int> ground_truth_faces_train;
-	vector<BoundingBox> bounding_boxs_train;
-	
-	vector<Mat_<uchar> > images_test;
-	vector<Mat_<float> > ground_truth_shapes_test;
-	vector<int> ground_truth_faces_test;
-	vector<BoundingBox> bounding_boxs_test;
+	omp_set_num_threads(16);
 
-	ifstream fin;
-	locale::global(locale(""));    //only for Chinese 
-	fin.open(listT);
-	locale::global(locale("C"));
+	string listT="list1.txt";
+	string listF="list2.txt";
+	
+	vector< Mat_<uchar> >		images_train;
+	vector< Mat_<float> >		ground_truth_shapes_train;
+	vector<int>					ground_truth_faces_train;
+	vector<BoundingBox>			bounding_boxs_train;
+	
+	ifstream fin;locale::global(locale(""));fin.open(listT);locale::global(locale("C"));
 	string line;
 	int sample_freq=0;
 	while(getline(fin,line)){
-		
 		sample_freq++;
 		printf("loading train:%d\r",sample_freq);
-		/*if (sample_freq%4!=0)
-		{
-			continue;
-		}
-		if (sample_freq>=200)
-		{
-			break;
-		}*/
 		istringstream stream(line);
 		string name;
 		stream >> name;
-		// Read Image
 		Mat_<uchar> image = imread(name,0);
-		if (image.rows<10||image.cols<10)
+		if (image.rows<MINHEIGHT||image.cols<MINHEIGHT||image.empty())
 		{
 			continue;
 		}
-		// Read ground truth shapes, you should rewrite it according to your samples
-		int shape28Index[28]={1,2,3,4,5,
-							10,11,12,13,14,
-							19,20,
-							27,28,
-							35,36,
-							37,38,
-							39,40,
-							41,42,
-							47,48,
-							49,50,
-							59,60};
+		// Read ground truth shapes
+		int shape28Index[28]={1,2,3,4,5,10,11,12,13,14,19,20,27,28,35,/*center 15th*/ 36 ,37,38,39,40,41,42,47,48,49,50,59,60};
+		bool badshape = false;
         Mat_<float> ground_truth_shape(28,2);
-    // we use 28 keypoints of 95 keypoints 
-		for (int i=0,j=0;i<95;++i)
-		{
-			if ( (i+1) == shape28Index[j])
-			{
+		for (int i=0,j=0;i<95;++i){
+			if ( (i+1) == shape28Index[j]){
 				stream >> ground_truth_shape(j,0);
 				stream >> ground_truth_shape(j,1);
-				//cout<<ground_truth_shape<<endl;
+				if (ground_truth_shape(j, 0)<0 || 
+					ground_truth_shape(j, 1)<0 ||
+					ground_truth_shape(j, 0)>=image.cols ||
+					ground_truth_shape(j, 1)>=image.rows)
+					badshape = true;
 				++j;				
-			}
-			else
-			{
-				float tmp;
-				stream >> tmp;
-				stream >> tmp;
+			}else{
+				float tmp1,tmp2;
+				stream >> tmp1;
+				stream >> tmp2;
+				/*if (tmp1 < 0 || tmp2 < 0 || tmp1 >= image.cols || tmp2 >= image.rows)
+					badshape = true;*/
 			}
 		}
+		if (badshape)
+			continue;
 		// Read Bounding box
-		BoundingBox bbx = CalculateBoundingBox2(ground_truth_shape);
-		adjustImage2(image,ground_truth_shape,bbx);
-		RNG random_generator(getTickCount());
-		if (random_generator.uniform(0.0,1.0)<(0.9))
-		{
-			images_train.push_back(image);
-			ground_truth_shapes_train.push_back(ground_truth_shape);   
-			ground_truth_faces_train.push_back(1);
-			bounding_boxs_train.push_back(bbx);
-			/*for(int i=0;i<ground_truth_shape.rows;i++){
-				circle(image,Point(ground_truth_shape(i,0),ground_truth_shape(i,1)),3,Scalar(255));
-			}
-			imshow("test2",image);
-			waitKey(0);*/
-		} 
-		else
-		{
-			images_test.push_back(image);
-			ground_truth_shapes_test.push_back(ground_truth_shape);   
-			ground_truth_faces_test.push_back(1);
-			bounding_boxs_test.push_back(bbx);
-			//rectangle(image,Point(new_box.start_x,new_box.start_y),Point(new_box.start_x+new_box.width,new_box.start_y+new_box.height),Scalar(255));
-			
-		}
+		BoundingBox bbx = CalculateBoundingBox(image, ground_truth_shape);
+		adjustImage(image, ground_truth_shape, bbx);
 
-
+		images_train.push_back(image);
+		ground_truth_shapes_train.push_back(ground_truth_shape);   
+		ground_truth_faces_train.push_back(1);
+		bounding_boxs_train.push_back(bbx);
 	}
 	fin.close();
 	cout<<endl;
 	int posLenth=images_train.size();
 	////////////////////////////////////////////////////////////////////////////////
-	ifstream fin2;
-	locale::global(locale(""));
-	fin2.open(listF);
-	locale::global(locale("C"));
-	line;
+	ifstream fin2;locale::global(locale(""));fin2.open(listF);locale::global(locale("C"));
 	sample_freq=0;
 	while(getline(fin2,line)){
-
-		sample_freq++;
-		printf("loading test:%d\r",sample_freq);
-		/*if (sample_freq%100!=0)
-		{
-			continue;
-		}*/
-		/*if (sample_freq>=500)
-		{
-			break;
-		}*/
-
-		istringstream stream(line);
-		string name;
-		stream >> name;
-		// Read Image
-		Mat_<uchar> image = imread(name,0);
-		
-		// due to memory size, we resize big img to small
-		if (image.rows>image.cols)
-		{
-			if (image.rows>MAXHEIGHT2)
-			{
-				float scale=1.0*MAXHEIGHT2/image.rows;
-				int new_width=image.cols*scale;
-				int new_height=image.rows*scale;
-				resize(image,image,Size(new_width,new_height));
-			} 
-		} 
+		istringstream stream(line);string name;stream >> name;
+		FILE* imgFile;imgFile=fopen(name.c_str(),"rb");
+		if (imgFile==NULL)
+			cout<<"error in read data\n";
 		else
 		{
-			if (image.cols>MAXHEIGHT2)
+			while (1)
 			{
-				float scale=1.0*MAXHEIGHT2/image.cols;
-				int new_width=image.cols*scale;
-				int new_height=image.rows*scale;
-				resize(image,image,Size(new_width,new_height));
+				sample_freq++;
+				printf("loading test:%d\r",sample_freq);
+				int jWidth;
+				int jHeight;
+				fread(&jWidth,4,1,imgFile);
+				fread(&jHeight,4,1,imgFile);
+				uchar* jData = new uchar[jWidth*jHeight];
+				fread(jData,1,jWidth*jHeight,imgFile);
+				
+				if (feof(imgFile)){
+					delete[] jData;
+					break;
+				}
+				if( jWidth<MINHEIGHT || jHeight<MINHEIGHT)
+					continue;
+				// Read Image
+				Mat_<uchar> image(jHeight,jWidth);
+				for (int k=0;k<jHeight;++k)
+					for (int  l=0;l<jWidth;++l)
+						image(k,l) = jData[k*jWidth+l];
+				delete[] jData;
+
+				if (image.rows > image.cols){
+					if (image.rows > MAXHEIGHT_NEG){
+						float scale = 1.0*MAXHEIGHT_NEG / image.rows;
+						int new_width = image.cols*scale;
+						int new_height = image.rows*scale;
+						resize(image, image, Size(new_width, new_height));
+					}
+				}else
+					if (image.cols > MAXHEIGHT_NEG){
+						float scale = 1.0*MAXHEIGHT_NEG / image.cols;
+						int new_width = image.cols*scale;
+						int new_height = image.rows*scale;
+						resize(image, image, Size(new_width, new_height));
+					}
+				BoundingBox new_box;
+				RNG random_generator(getTickCount());
+				int random_idx = random_generator.uniform(0, posLenth - 1);
+				getRandomBox(image, bounding_boxs_train[random_idx], new_box);
+				Mat_<float> temp = ProjectShape(ground_truth_shapes_train[random_idx], bounding_boxs_train[random_idx]);
+				Mat_<float> ground_truth_shape = ReProjectShape(temp, new_box);
+				images_train.push_back(image);
+				ground_truth_shapes_train.push_back(ground_truth_shape);   
+				ground_truth_faces_train.push_back(-1);
+				bounding_boxs_train.push_back(new_box);
 			}
 		}
-		
-		// Read ground truth shapes
-		RNG random_generator(getTickCount());
-		int random_idx = random_generator.uniform(0,posLenth-1);
-		// random box
-		BoundingBox new_box;
-		int img_height=image.rows;
-		int img_width=image.cols;
-
-		int box_height=img_height;
-		int box_width=img_width;
-		do 
-		{
-			if (img_height>MINHEIGHT)
-			{
-				box_height= random_generator.uniform(MINHEIGHT*1.0,(double)img_height*0.8);
-			}
-			box_width = box_height*bounding_boxs_train[random_idx].width/bounding_boxs_train[random_idx].height;
-		} while (box_width > img_width*0.8);
-		
-		int box_x = random_generator.uniform(0.0+0.2*img_width,(double)img_width*0.8-box_width);
-		int box_y = random_generator.uniform(0.0+0.2*img_height,(double)img_height*0.8-box_height);
-		new_box.start_x=box_x;
-		new_box.start_y=box_y;
-		new_box.width=box_width;
-		new_box.height=box_height;
-		new_box.centroid_x=box_x+box_width/2.0;
-		new_box.centroid_y=box_y+box_height/2.0;
-
-
-		Mat_<float> temp = ProjectShape(ground_truth_shapes_train[random_idx], bounding_boxs_train[random_idx]);
-		Mat_<float> ground_truth_shape = ReProjectShape(temp, new_box);
-
-		/*rectangle(image,Point(new_box.start_x,new_box.start_y),Point(new_box.start_x+new_box.width,new_box.start_y+new_box.height),Scalar(255));
-		for(int i=0;i<ground_truth_shape.rows;i++){
-			circle(image,Point(ground_truth_shape(i,0),ground_truth_shape(i,1)),3,Scalar(255));
-		}
-		imshow("test2",image);
-		waitKey(0);*/
-
-		if (random_generator.uniform(0.0,1.0)<(0.9))
-		{
-			images_train.push_back(image);
-			ground_truth_shapes_train.push_back(ground_truth_shape);   
-			ground_truth_faces_train.push_back(-1);
-			bounding_boxs_train.push_back(new_box);
-		} 
-		else
-		{
-			images_test.push_back(image);
-			ground_truth_shapes_test.push_back(ground_truth_shape);   
-			ground_truth_faces_test.push_back(-1);
-			bounding_boxs_test.push_back(new_box);
-		}
+		fclose(imgFile);
 	}
 	fin2.close();
-
-	cout<<endl;
-
-	cout<<"train num:"<<images_train.size()<<" ,pos is "<<posLenth<<endl;
-	cout<<"test num:"<<images_test.size()<<endl;
+	cout<<"\ntrain num:"<<images_train.size()<<" ,pos is "<<posLenth<<endl;
+	
 
 	/*LBFRegressor regressor;
-	regressor.Train(images_train,ground_truth_shapes_train,ground_truth_faces_train,bounding_boxs_train);
-	regressor.Save(modelPath+"LBF6.model");*/
+	regressor.Train(images_train, ground_truth_shapes_train, ground_truth_faces_train, bounding_boxs_train, posLenth);
+	regressor.Save(modelPath);*/
 	
 	LBFRegressor regressor;
-	regressor.Load(modelPath+"LBF6.model");
+	regressor.Load(modelPath);
 
-  // have not finished the test operation
-	Mat_<uchar> testImg=imread("F:\\test.jpg",0);
+	Mat_<uchar> testImg=imread("img\\3.jpg",0);
+	Mat_<uchar> forshow = testImg.clone();
 	float scale=1.1;
-	int minSize=50/scale;
+	int minSize=40/scale;
 	float shuffle=0.1;
 	int ccc=0;
 	int ttt=0;
@@ -278,11 +179,12 @@ int main( int argc, const char** argv ){
 				Mat_<float>shapes = regressor.Predict(tmp,bx,1,result_face,count);
 				if (result_face)
 				{
-					/*for (int m=0;m<shapes.rows;++m)
+					for (int m=0;m<shapes.rows;++m)
 					{
-						circle(tmp,Point(shapes(m,0),shapes(m,1)),3,Scalar(255));
+						//circle(tmp,Point(shapes(m,0),shapes(m,1)),3,Scalar(255));
+						circle(forshow,Point(left_x+shapes(m,0),top_y+shapes(m,1)),3,Scalar(255));
 					}
-					imshow("cur",tmp);
+					/*imshow("cur",tmp);
 					waitKey(0);*/
 				}
 				else
@@ -290,111 +192,112 @@ int main( int argc, const char** argv ){
 			}
 		}
 	}
+	imshow("cur", forshow);waitKey(0);
 	cout<<"ccc:"<<ccc<<endl;
 	cout<<"ttt:"<<ttt<<endl;
 
-	
-	int right_num_pos=0,right_num_neg=0,pos_num=0,neg_num=0;
-	vector<Mat_<float>> current_shapes;
-	vector<Mat_<float>> real_shapes;
-	vector<int> real_idx;
-	int count=0;
-	for (int i=0;i<images_test.size();++i)
-	{
-		bool result_face=true;
-		int fcount=0;
-		Mat_<float>shapes = regressor.Predict(images_test[i],bounding_boxs_test[i],1,result_face,fcount);
-		if (!result_face)
-		{
-			count+=fcount;
-		}
-		
-		if (1==ground_truth_faces_test[i] && result_face==true)
-		{
-			right_num_pos++;
-		}
-		if (-1==ground_truth_faces_test[i] && result_face==false)
-		{
-			right_num_neg++;
-		}
-		if (ground_truth_faces_test[i]==1 && result_face==true)
-		{
-			current_shapes.push_back(shapes);
-			real_shapes.push_back(ground_truth_shapes_test[i]);
-			real_idx.push_back(i);
-		}
-		if (ground_truth_faces_test[i]==1)
-		{
-			pos_num++;
-		} 
-		else
-		{
-			neg_num++;
-		}
-		/*if(ground_truth_faces_test[i]==1 && result_face==false)
-		{
-			Mat_<uchar> tmp=images_test[i].clone();
-			for (int j=0;j<ground_truth_shapes_test[i].rows;++j)
-			{
-				circle(tmp,Point(ground_truth_shapes_test[i](j,0),ground_truth_shapes_test[i](j,1)),3,Scalar(255));
-			}
-			imshow("real",tmp);
-			waitKey(0);
+	//
+	//int right_num_pos=0,right_num_neg=0,pos_num=0,neg_num=0;
+	//vector<Mat_<float>> current_shapes;
+	//vector<Mat_<float>> real_shapes;
+	//vector<int> real_idx;
+	//int count=0;
+	//for (int i=0;i<images_test.size();++i)
+	//{
+	//	bool result_face=true;
+	//	int fcount=0;
+	//	Mat_<float>shapes = regressor.Predict(images_test[i],bounding_boxs_test[i],1,result_face,fcount);
+	//	if (!result_face)
+	//	{
+	//		count+=fcount;
+	//	}
+	//	
+	//	if (1==ground_truth_faces_test[i] && result_face==true)
+	//	{
+	//		right_num_pos++;
+	//	}
+	//	if (-1==ground_truth_faces_test[i] && result_face==false)
+	//	{
+	//		right_num_neg++;
+	//	}
+	//	if (ground_truth_faces_test[i]==1 && result_face==true)
+	//	{
+	//		current_shapes.push_back(shapes);
+	//		real_shapes.push_back(ground_truth_shapes_test[i]);
+	//		real_idx.push_back(i);
+	//	}
+	//	if (ground_truth_faces_test[i]==1)
+	//	{
+	//		pos_num++;
+	//	} 
+	//	else
+	//	{
+	//		neg_num++;
+	//	}
+	//	/*if(ground_truth_faces_test[i]==1 && result_face==false)
+	//	{
+	//		Mat_<uchar> tmp=images_test[i].clone();
+	//		for (int j=0;j<ground_truth_shapes_test[i].rows;++j)
+	//		{
+	//			circle(tmp,Point(ground_truth_shapes_test[i](j,0),ground_truth_shapes_test[i](j,1)),3,Scalar(255));
+	//		}
+	//		imshow("real",tmp);
+	//		waitKey(0);
 
-			Mat_<uchar> tmp2=images_test[i].clone();
-			for (int j=0;j<shapes.rows;++j)
-			{
-				circle(tmp2,Point(shapes(j,0),shapes(j,1)),3,Scalar(255));
-			}
-			imshow("cur",tmp2);
-			waitKey(0);
-		}
-		if(ground_truth_faces_test[i]==-1 && result_face==true)
-		{
-			Mat_<uchar> tmp=images_test[i].clone();
-			for (int j=0;j<ground_truth_shapes_test[i].rows;++j)
-			{
-				circle(tmp,Point(ground_truth_shapes_test[i](j,0),ground_truth_shapes_test[i](j,1)),3,Scalar(255));
-			}
-			imshow("real",tmp);
-			waitKey(0);
+	//		Mat_<uchar> tmp2=images_test[i].clone();
+	//		for (int j=0;j<shapes.rows;++j)
+	//		{
+	//			circle(tmp2,Point(shapes(j,0),shapes(j,1)),3,Scalar(255));
+	//		}
+	//		imshow("cur",tmp2);
+	//		waitKey(0);
+	//	}
+	//	if(ground_truth_faces_test[i]==-1 && result_face==true)
+	//	{
+	//		Mat_<uchar> tmp=images_test[i].clone();
+	//		for (int j=0;j<ground_truth_shapes_test[i].rows;++j)
+	//		{
+	//			circle(tmp,Point(ground_truth_shapes_test[i](j,0),ground_truth_shapes_test[i](j,1)),3,Scalar(255));
+	//		}
+	//		imshow("real",tmp);
+	//		waitKey(0);
 
-			Mat_<uchar> tmp2=images_test[i].clone();
-			for (int j=0;j<shapes.rows;++j)
-			{
-				circle(tmp2,Point(shapes(j,0),shapes(j,1)),3,Scalar(255));
-			}
-			imshow("cur",tmp2);
-			waitKey(0);
-		}*/
-		
-	}
-	cout<<"average compare in false samples:"<<count*1.0/neg_num<<endl;
-	float classification=(right_num_neg+right_num_pos)*1.0/images_test.size();
-	//cout<<"total right rate:"<<classification<<endl;
-	cout<<"pos right rate:"<<right_num_pos*1.0/pos_num<<endl;
-	cout<<"neg error rate:"<<1-right_num_neg*1.0/neg_num<<endl;
-	float MRSE_sum = 0;
-	for (int i =0; i<real_idx.size();i++){
-		MRSE_sum += CalculateError(real_shapes[i], current_shapes[i]);
-		
-		Mat_<uchar> tmp=images_test[real_idx[i]].clone();
-		for (int j=0;j<real_shapes[i].rows;++j)
-		{
-			circle(tmp,Point(real_shapes[i](j,0),real_shapes[i](j,1)),3,Scalar(255));
-		}
-		imshow("real",tmp);
-		waitKey(0);
+	//		Mat_<uchar> tmp2=images_test[i].clone();
+	//		for (int j=0;j<shapes.rows;++j)
+	//		{
+	//			circle(tmp2,Point(shapes(j,0),shapes(j,1)),3,Scalar(255));
+	//		}
+	//		imshow("cur",tmp2);
+	//		waitKey(0);
+	//	}*/
+	//	
+	//}
+	//cout<<"average compare in false samples:"<<count*1.0/neg_num<<endl;
+	//float classification=(right_num_neg+right_num_pos)*1.0/images_test.size();
+	////cout<<"total right rate:"<<classification<<endl;
+	//cout<<"pos right rate:"<<right_num_pos*1.0/pos_num<<endl;
+	//cout<<"neg error rate:"<<1-right_num_neg*1.0/neg_num<<endl;
+	//float MRSE_sum = 0;
+	//for (int i =0; i<real_idx.size();i++){
+	//	MRSE_sum += CalculateError(real_shapes[i], current_shapes[i]);
+	//	
+	//	Mat_<uchar> tmp=images_test[real_idx[i]].clone();
+	//	for (int j=0;j<real_shapes[i].rows;++j)
+	//	{
+	//		circle(tmp,Point(real_shapes[i](j,0),real_shapes[i](j,1)),3,Scalar(255));
+	//	}
+	//	imshow("real",tmp);
+	//	waitKey(0);
 
-		Mat_<uchar> tmp2=images_test[real_idx[i]].clone();
-		for (int j=0;j<current_shapes[i].rows;++j)
-		{
-			circle(tmp2,Point(current_shapes[i](j,0),current_shapes[i](j,1)),3,Scalar(255));
-		}
-		imshow("cur",tmp2);
-		waitKey(0);
-	}
-	cout << "Mean Root Square Error is "<< MRSE_sum/current_shapes.size()*100 <<"%"<<endl;
+	//	Mat_<uchar> tmp2=images_test[real_idx[i]].clone();
+	//	for (int j=0;j<current_shapes[i].rows;++j)
+	//	{
+	//		circle(tmp2,Point(current_shapes[i](j,0),current_shapes[i](j,1)),3,Scalar(255));
+	//	}
+	//	imshow("cur",tmp2);
+	//	waitKey(0);
+	//}
+	//cout << "Mean Root Square Error is "<< MRSE_sum/current_shapes.size()*100 <<"%"<<endl;
 
 	system("pause");
 	return 0;
@@ -403,15 +306,14 @@ int main( int argc, const char** argv ){
 void InitializeGlobalParam(){
     global_params.bagging_overlap = 0.4;
     global_params.max_numtrees = 10;
-    global_params.max_depth = 6;
+    global_params.max_depth = 4;
     global_params.landmark_num = 28;
     global_params.initial_num = 1;
-    
     global_params.max_numstage = 5;
-    float m_max_radio_radius[10] = {0.4+0.,0.3+0.,0.2+0.,0.15+0.0, 0.12+0.0, 0.10+0.0, 0.08+0.0, 0.06+0.0, 0.06+0.0,0.05+0.0};
-    float m_max_numfeats[10] = {1000+00, 1000+00, 1000+00, 1000+00, 1000+00, 300, 200,200,100,100};
+
+    float m_max_radio_radius[10] = {1.0, 0.8, 0.4, 0.2, 0.1, 0.10, 0.08, 0.06, 0.06,0.05};
+    float m_max_numfeats[10] = {2000, 2000, 2000, 2000, 2000, 300, 200,200,100,100};
 	float m_max_probility[10] = {0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0};
-	//float m_max_probility[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0.0};
 
     for (int i=0;i<10;i++){
         global_params.max_radio_radius[i] = m_max_radio_radius[i];
@@ -422,7 +324,7 @@ void InitializeGlobalParam(){
 	for (int i=0;i<10;i++){
 		global_params.max_probility[i] = m_max_probility[i];
 	}
-    global_params.max_numthreshs = 2000;
+    global_params.max_numthreshs = 24;
 }
 
 void ReadGlobalParamFromFile(string path){
@@ -442,19 +344,10 @@ void ReadGlobalParamFromFile(string path){
     for (int i = 0; i< global_params.max_numstage; i++){
         fin >> global_params.max_radio_radius[i];
     }
-    
     for (int i = 0; i < global_params.max_numstage; i++){
         fin >> global_params.max_numfeats[i];
     }
     cout << "Loading GlobalParam end"<<endl;
     fin.close();
 }
-void PrintHelp(){
-    cout << "Useage:"<<endl;
-    cout << "1. train your own model:    LBF.out  TrainModel "<<endl;
-    cout << "2. test model on dataset:   LBF.out  TestModel"<<endl;
-    cout << "3. test model via a camera: LBF.out  Demo "<<endl;
-    cout << "4. test model on a pic:     LBF.out  Demo xx.jpg"<<endl;
-    cout << "5. test model on pic set:   LBF.out  Demo Img_Path.txt"<<endl;
-    cout << endl;
-}
+
